3efcf042974d9290ed92750c4ca234cba6e799f4
Romane Gourlet <romanemgourlet@gmail.com>
2026-06-15 16:42:56 +0200

    Adding readbility score computations

--- readability.py
+++ readability.py
@@ -8,4 +8,12 @@ SMOG Index
 Dale-Chall
 """
 
+with open("./data/forms.txt", "r") as f:
+    text = f.read()
 
+print(f"Flesch Reading ease : {textstat.flesch_reading_ease(text)}")
+print(f"Flesch-Kincaid Grade Level: {textstat.flesch_kincaid_grade(text)}")
+print(f"SMOF Index : {textstat.smog_index(text)}")
+print(f"Gunning Fox : {textstat.gunning_fog(text)}")
+print(f"Dale-Chall : {textstat.dale_chall_readability_score(text)}")
+print(f"Difficult words : {textstat.difficult_words(text)}")
