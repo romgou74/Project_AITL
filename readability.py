a8dc4155fa51be8ee31168f7503f1fa89236b6af
Romane Gourlet <romanemgourlet@gmail.com>
2026-06-15 18:30:53 +0200

    Created a ratio for difficult words instead of having simply the amount of difficult words

--- readability.py
+++ readability.py
@@ -1,4 +1,5 @@
 import textstat
+import re
 
 """
 Flesch Reading Ease
@@ -11,9 +12,24 @@ Dale-Chall
 with open("./data/forms.txt", "r") as f:
     text = f.read()
 
+file = re.sub(r'<[^>]+>', '', text)  # remove HTML tags
+lst = file.split('\n')
+lst = [re.sub(r'\s+', ' ', line).strip() for line in lst]
+
+for i in range (len(lst)) :
+    if lst[i] and lst[i][-1] != "." :
+        lst[i] += "."
+file = " ".join(lst)
+
+file = re.sub(r'\s+', ' ', file).strip()  # normalize whitespace
+cleaned = file.lower()
+cleaned = re.sub(r"[^\w\s]", "", text)
+cleaned = cleaned.split()
+print(len(cleaned))
+
 print(f"Flesch Reading ease : {textstat.flesch_reading_ease(text)}")
 print(f"Flesch-Kincaid Grade Level: {textstat.flesch_kincaid_grade(text)}")
 print(f"SMOF Index : {textstat.smog_index(text)}")
 print(f"Gunning Fox : {textstat.gunning_fog(text)}")
 print(f"Dale-Chall : {textstat.dale_chall_readability_score(text)}")
-print(f"Difficult words : {textstat.difficult_words(text)}")
+print(f"Difficult words : {textstat.difficult_words(text)/len(cleaned)}")
