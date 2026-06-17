ecc1b75411fcb7fda492fe130cdf2f1cdd12ace0
Romane Gourlet <romanemgourlet@gmail.com>
2026-06-17 12:18:40 +0200

    Making it into functions instead of prints

--- readability.py
+++ readability.py
@@ -1,6 +1,7 @@
 import textstat
 import re
 import sys
+import csv
 
 """
 Flesch Reading Ease
@@ -29,9 +30,31 @@ cleaned = file.lower()
 cleaned = re.sub(r"[^\w\s]", "", text)
 cleaned = cleaned.split()
 
+def get_flesch(text):
+    return textstat.flesch_reading_ease(text)
+
+def get_flesch_reading(text):
+    return textstat.flesch_reading_ease(text)
+
+def get_flesch_kincaid(text):
+    return textstat.flesch_kincaid_grade(text)
+
+def get_smog(text):
+    return textstat.smog_index(text)
+
+def get_gunning(text):
+    return textstat.gunning_fog(text)
+
+def get_dale(text):
+    return textstat.dale_chall_readability_score(text)
+
+def get_difficult(text, cleaned):
+    return textstat.difficult_words(text) / len(cleaned)
+
 print(f"Flesch Reading ease : {textstat.flesch_reading_ease(text)}")
 print(f"Flesch-Kincaid Grade Level: {textstat.flesch_kincaid_grade(text)}")
 print(f"SMOF Index : {textstat.smog_index(text)}")
 print(f"Gunning Fox : {textstat.gunning_fog(text)}")
 print(f"Dale-Chall : {textstat.dale_chall_readability_score(text)}")
 print(f"Difficult words : {textstat.difficult_words(text)/len(cleaned)}")
+
